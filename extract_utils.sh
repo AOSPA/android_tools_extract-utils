6e85a4fe946f5f172dd931c201cecc62e51353f4
Michael Bestas <mkbestas@lineageos.org>
2024-10-13 22:41:43 +0300

    Switch to SPDX copyright headers
    Change-Id: Ibb2c18ac5975fc619120c47ba1c6b2e4e5b5b1c2

--- extract_utils.sh
+++ extract_utils.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 #
-# Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2024 The LineageOS Project
-# Copyright (C) 2017-2020 Paranoid Android
-#
+# SPDX-FileCopyrightText: 2016 The CyanogenMod Project
+# SPDX-FileCopyrightText: 2017-2024 The LineageOS Project
+# SPDX-FileCopyrightText: 2017-2020 Paranoid Android
 # SPDX-License-Identifier: Apache-2.0
 #
 
