a7afb6717b615f6a70ded1f9c1941d00102958dc
Jyotiraditya Panda <jyotiraditya@aospa.co>
2025-04-14 19:21:18 +0530

    extract_utils: implement split_file_parts() blob fixup
    Change-Id: I71420b44101f2ca4dfa47cfe82c5b9c6c4ea2b51
    Signed-off-by: Jyotiraditya Panda <jyotiraditya@aospa.co>

--- extract_utils/file.py
+++ extract_utils/file.py
@@ -55,6 +55,7 @@ class FileArgs(str, Enum):
     FIX_SONAME = 'FIX_SONAME'
     FIX_XML = 'FIX_XML'
     STRIP_DEBUG_SECTIONS = 'STRIP_DEBUG_SECTIONS'
+    SPLIT_FILE = 'SPLIT_FILE'
     OVERRIDES = 'OVERRIDES'
     PRESIGNED = 'PRESIGNED'
     REQUIRED = 'REQUIRED'
@@ -79,6 +80,7 @@ FILE_ARGS_TYPE_MAP = {
     FileArgs.FIX_SONAME: True,
     FileArgs.FIX_XML: True,
     FileArgs.STRIP_DEBUG_SECTIONS: True,
+    FileArgs.SPLIT_FILE: str,
     FileArgs.OVERRIDES: list,
     FileArgs.PRESIGNED: True,
     FileArgs.REQUIRED: list,
