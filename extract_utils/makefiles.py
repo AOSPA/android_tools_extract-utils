19f4d51ffc4ab945ed0bfb173134c44b992e659d
Jyotiraditya Panda <jyotiraditya@aospa.co>
2025-09-05 21:08:26 +0530

    extract_utils: add overrides support for prebuilt modules
    Add overrides property support for cc_prebuilt_binary,
    cc_prebuilt_library_shared, prebuilt_apex, dex_import,
    prebuilt_etc, and cc_library_shared modules.
    
    Previously only android_app_import supported overrides.
    However, build/soong/androidmk shows LOCAL_OVERRIDES_MODULES
    is converted to "overrides" for all these module types.
    
    Change-Id: Idbd453fdac85f65cdf0d82c8f461651062338812
    Signed-off-by: Jyotiraditya Panda <jyotiraditya@aospa.co>

--- extract_utils/makefiles.py
+++ extract_utils/makefiles.py
@@ -251,6 +251,7 @@ def write_elfs_package(
             .no_strip()
             .prefer()
             .relative_install_path()
+            .set('overrides', file.overrides, optional=True)
             .specific()
             .recovery_available()
             .set('required', file.required, optional=True)
@@ -269,6 +270,7 @@ def write_elfs_package(
         .check_elf(enable_check_elf)
         .relative_install_path()
         .prefer()
+        .set('overrides', file.overrides, optional=True)
         .specific()
         .recovery_available()
         .set('required', file.required, optional=True)
@@ -341,6 +343,7 @@ def write_apex_package(file: File, builder: FileBpBuilder):
         .owner()
         .src()
         .filename()
+        .set('overrides', file.overrides, optional=True)
         .specific()
     )
     return package_name
@@ -376,6 +379,7 @@ def write_framework_package(file: File, builder: FileBpBuilder):
         .name(package_name)
         .owner()
         .jars()
+        .set('overrides', file.overrides, optional=True)
         .specific()
     )
 
@@ -397,6 +401,7 @@ def write_etc_package(file: File, builder: FileBpBuilder):
         .src()
         .set('filename_from_src', True)
         .sub_dir()
+        .set('overrides', file.overrides, optional=True)
         .specific()
         .recovery_available()
     )
@@ -680,6 +685,7 @@ def write_dummy_shared_libs_packages(
         .name(package_name)
         .stem(stem)
         .multilibs(bitses)
+        .set('overrides', file.overrides, optional=True)
         .specific()
     )
 
